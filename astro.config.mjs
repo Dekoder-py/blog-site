b129e06acda41136652d37032559784a42f3a3a5
Dekoder-py <dekod3r.py@gmail.com>
2025-10-21 17:47:31 +1300

    create astro

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://kyle-is-a.dino.icu",
+  integrations: [mdx(), sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
