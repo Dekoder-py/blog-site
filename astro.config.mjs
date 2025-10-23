fdedacec044fe43aaa1e84e518c3ccd2627356ef
Dekoder-py <dekod3r.py@gmail.com>
2025-10-24 06:50:22 +1300

    updates
    ITS MY BIRTHDAY UPDATE

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kyle-is-a.dino.icu",
+  site: "https://blog.codingcorner.dev",
   integrations: [mdx(), sitemap()],
   adapter: vercel({
     webAnalytics: {
